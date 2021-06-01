9e00ecf0c8145481dc2933f33f0668f43970a989
Ayush Verma <78836695+AyushVerma-code@users.noreply.github.com>
2021-06-01 08:47:59 +0530

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a;
+    printf("Enter a Character");
+    scanf("%c",&a);
+    printf("The ASCII value is ");
+    printf("%d",a);
+    return 0;
+}
