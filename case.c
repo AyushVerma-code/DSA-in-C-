c5a1ebb2dc372ff2ab1025b21ae288039b833ac7
Ayush Verma <78836695+AyushVerma-code@users.noreply.github.com>
2021-06-01 08:48:22 +0530

    Create case.c

---
+++ case.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a;
+    printf("Enter Character");
+    scanf("%c", &a);
+    char b= a+32;
+    printf("%c",b);
+    return 0;
+}
