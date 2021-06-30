980aca5572fa507402363f47ae83226eab84c334
Ayush Verma <78836695+AyushVerma-code@users.noreply.github.com>
2021-06-30 11:42:14 +0530

    Create removeX.cpp

---
+++ recursion/removeX.cpp
@@ -0,0 +1,24 @@
+void removeX(char s[])
+{
+   if (s[0] == '\0') 
+   {
+    return;
+   }
+   if (s[0] != 'x') 
+   { 
+    removeX(s + 1); 
+   } 
+  else 
+  {
+      int i=1;
+      for (; s[i] != '\0'; i++)
+      {
+        s[i - 1] = s[i];
+      }   
+        s[i - 1] = s[i];
+        removeX(s);
+  }
+    
+}
+
+   
