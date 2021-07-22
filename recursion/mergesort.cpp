b3ba75f0184f0ccd854a218532a355c324b4bd77
Ayush Verma <78836695+AyushVerma-code@users.noreply.github.com>
2021-07-22 09:54:42 +0530

    Create mergesort.cpp

---
+++ recursion/mergesort.cpp
@@ -0,0 +1,76 @@
+void msh(int arr[], int l, int m, int r)
+{
+    int k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+ 
+    // Create temp arrays
+    int L[n1], R[n2];
+ 
+    // Copy data to temp arrays L[] and R[]
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for(int j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+ 
+    // Merge the temp arrays
+    // back into arr[l..r]
+    int i = 0;
+    int j = 0;
+    k = l;
+     
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements
+    // of L[], if there are any
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    // Copy the remaining elements
+    // of R[], if there are any
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void ms1(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+         
+        // Same as (l+r)/2 but avoids
+        // overflow for large l & h
+        int m = l + (r - l) / 2;
+        ms1(arr, l, m);
+        ms1(arr, m + 1, r);
+        msh(arr, l, m, r);
+    }
+}
+ 
+// Function to merge the two haves arr[l..m]
+// and arr[m+1..r] of array arr[]
+ 
+void mergeSort(int input[], int size)
+{
+    ms1(input,0,size-1);
+}
+ 
